4fb263c84847f3019bd4ee0d7c54aa17065713f4
faizalbaihaqiardithoriqoh <baihaqijovanca@gmail.com>
2023-07-16 15:29:03 +0700

    home.dart Fixes #9 saya telah mengerjakan home.dart by faizal

---
+++ lib/Screen/home.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:pencacatantugas/models/materipelajaran.dart';
+import 'package:pencacatantugas/screen/matapelajaran.dart';
+import 'package:pencacatantugas/screen/profile.dart';
+import '../models/tugas.dart';
+import './tugasharian.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  late List<Tugas> _listTugas; 
+  late List<MateriPelajaran> _listMataPelajaran; 
+
+  List<Widget> tabs = []; 
+
+  @override
+  void initState() {
+    _listTugas = [
+      Tugas(id: '1', matakuliah: 'Pemrograman Mobile', deskripsi: 'Ngoding', status: 'false')
+    ];
+
+    _listMataPelajaran = [
+      MateriPelajaran(id: '1', matakuliah: 'Pemrograman Mobile', deskripsi: 'Flutter adalah blablabla')
+    ];
+
+    tabs = [
+      TugasHarian(listTugas: _listTugas.toList()),
+      CatatanMateri(listPelajaran: _listMataPelajaran.toList()),
+      ProfileScreen()
+    ];
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: tabs,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Materi Pelajaran',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
